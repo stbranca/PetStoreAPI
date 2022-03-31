cbdad6629946f266571531a2b0a825d23e2d3d04
stbranca <eldermelobatista@gmail.com>
2022-03-31 11:11:07 -0300

    Finalizado o script excluirPet com Delete.

--- src/test/java/petstore/Pets.java
+++ src/test/java/petstore/Pets.java
@@ -23,7 +23,7 @@ public class Pets {
     // Include - Create - Post
     @Test(priority = 1)
     public void incluirPet() throws IOException {
-        String jsonBody = lerJson("db/pet.json");
+        String jsonBody = lerJson("db/pet1.json");
 
         // Sintaxe Gherkin
         //Given - When - Then
@@ -67,6 +67,43 @@ public class Pets {
                 .path("category.name")
                 ;
         System.out.println("O token é " + token);
+    }
+    @Test(priority = 3)
+    public void alterarPet() throws IOException {
+        String jsonBody = lerJson("db/pet2.json");
 
+        given()
+                .contentType("application/json")
+                .log().all()
+                .body(jsonBody)
+
+                .when()
+                .put(uri)
+
+                .then()
+                .log().all()
+                .statusCode(200)
+                .body("name", is("Marley"))
+                .body("status", is("sold"))
+                ;
+    }
+    @Test(priority = 4)
+    public void excluirPet(){
+        String petId = "1947090936";
+
+        given()
+                .contentType("application/json")
+                .log().all()
+
+                .when()
+                .delete(uri + "/" + petId)
+
+                .then()
+                .log().all()
+                .statusCode(200)
+                .body("code", is(200))
+                .body("type", is("unknown"))
+                .body("message", is(petId))
+        ;
     }
 }
